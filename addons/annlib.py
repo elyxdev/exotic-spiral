4715397f8362a12729d06e4e7099111d843d526c
Elyx <151476172+elyxdev@users.noreply.github.com>
2024-06-20 20:27:20 +0000

    updt

---
+++ addons/annlib.py
@@ -0,0 +1,12 @@
+import time; from pystyle import Colors, Write
+def logo():
+    global degmain
+    degmain=Colors.red_to_purple
+    Write.Print("""
+┏┓    ┓•┓   ┓ ┏┓
+┣┫┏┓┏┓┃┓┣┓  ┃ ┃┫
+┛┗┛┗┛┗┗┗┗┛  ┻•┗┛                                             
+""", Colors.red_to_yellow, 0.009)
+    Write.Print("✧  Custom MSP loader", Colors.red_to_yellow, 0.05)
+    print()
+    time.sleep(2.5)
