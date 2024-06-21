ebc2e485ac2afccd2041a0026992b82719f57175
Elyx <151476172+elyxdev@users.noreply.github.com>
2024-06-21 03:57:23 +0000

    Mundo guardado 2024-06-21

--- probe.py
+++ probe.py
@@ -1,13 +1,11 @@
-import os, subprocess
-def obtener_salida_comando(comando):
-    proceso = subprocess.Popen(comando, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    salida, error = proceso.communicate()  # Obtener la salida y los errores del proceso
-    salida_decodificada = salida.decode('utf-8')
-    error_decodificado = error.decode('utf-8')
-    return salida_decodificada, error_decodificado
-
-res,err = obtener_salida_comando("git lfs ls-files")
-print(res, res=="")
-os.system('git lfs track "*.zip"')
-res,err = obtener_salida_comando("git lfs ls-files")
-print(res, res=="")
+import requests
+import urllib3
+from requests.exceptions import ConnectionError
+try:
+    requests.get("https://serveo.net")
+except urllib3.exceptions.MaxRetryError as e:
+    print(f"Maximos retries excedidos: {e}")
+except urllib3.exceptions.NewConnectionError as e:
+    print(f"Error al conectar: {e}")
+except ConnectionError as e:
+    print(f"Error al conectar 2: {e}")
